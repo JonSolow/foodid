a503745b3288cbc30e8e62e20e26834c9a4d8cea
JonSolow <jssolow+gh@gmail.com>
2019-07-29 14:54:59 -0400

    Remove unused files

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# __init__.py
-
-# Future task - place core local imports here
-# and perform other initialization operations.
--- src/model.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# model
-
-# code for running ml model goes here.
-
-def test_model():
-    print('In Model')
-    pass
